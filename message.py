aca0dbd45d907e88d9028125a361ae7cc7134417
100nout.by <seo.geflih@gmail.com>
2022-08-25 00:35:04 +0300

    add 001: delete

--- message.py
+++ message.py
@@ -57,13 +57,6 @@ def add_message(sender, text):
     all_messages.append(new_message)
 
 
-@app.route("/len_text")
-def add_len(text):
-    text = len(text)
-
-    return len(txt)
-
-
 @app.route("/message_count")
 def message_count():
     return f"There's {len(all_messages)} messages in our chat."
